7b27f217ac6a25d659e11ad2cebaa4ac3fbf21f7
jonx1183 <jonx1183@stud.kea.dk>
2022-05-06 09:32:01 +0200

    To a. Done

---
+++ src/To/ExTo.java
@@ -0,0 +1,20 @@
+package To;
+
+import java.util.Scanner;
+
+public class ExTo {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //int row = sc.nextInt();
+//Hver gang det ydre loop køres så looper det indre loop igennem indtil dens krav er opfyldt.
+// Den vil derefter køre det ydre loop igen og fortsætte indtil dens krav er opfyldt.
+    for (int i = 0; i <= 4; i++){
+
+      for (int test = 0; test < i; test++ ){
+        System.out.print('*');
+      }
+
+      System.out.println();
+    }
+  }
+}
