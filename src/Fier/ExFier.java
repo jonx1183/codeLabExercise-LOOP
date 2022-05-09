350f8b53433dbe540d2488b0d96cbb3e981b75bc
jonx1183 <jonx1183@stud.kea.dk>
2022-05-09 09:48:53 +0200

    Fire is done

---
+++ src/Fier/ExFier.java
@@ -0,0 +1,11 @@
+package Fier;
+
+public class ExFier {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 255; i++){
+      System.out.println("The ASCII value of " + (char)i + " is "+ i);
+    }
+  }
+}
