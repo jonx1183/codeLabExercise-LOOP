310177ef2d3e9acf6656ebf364b8398885555471
jonx1183 <jonx1183@stud.kea.dk>
2022-05-09 09:27:26 +0200

    Tre is done

--- src/To/ExTo.java
+++ src/To/ExTo.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class ExTo {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    //int row = sc.nextInt();
+
+    //a.
+
 //Hver gang det ydre loop køres så looper det indre loop igennem indtil dens krav er opfyldt.
 // Den vil derefter køre det ydre loop igen og fortsætte indtil dens krav er opfyldt.
+
     for (int i = 0; i <= 4; i++){
 
       for (int test = 0; test < i; test++ ){
-        System.out.print('*');
+        System.out.print("*");
       }
 
       System.out.println();
---
+++ src/Tre/ExTre.java
@@ -0,0 +1,11 @@
+package Tre;
+
+public class ExTre {
+
+  public static void main(String[] args) {
+   int n = 24;
+    for (int i = 1; i < 10; i++){
+      System.out.println(n*i);
+    }
+  }
+}
